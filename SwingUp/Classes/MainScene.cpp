43c2d045e2f02ee15546637813488347077774ee
Deniz Saner <deniz.saner@rwth-aachen.de>
2017-03-20 11:27:01 +0100

    Implemented fully functional box2d world of the cart-pole swingup scenario. Added angle measurement and normalization

--- SwingUp/Classes/MainScene.cpp
+++ SwingUp/Classes/MainScene.cpp
@@ -3,7 +3,10 @@
 #include "ui/CocosGUI.h"
 #include "Agent.h"
 #include "AgentReader.h"
+#include "RailReader.h"
+#include "Rail.h"
 #include <iostream>
+#include "math.h"
 
 USING_NS_CC;
 
@@ -35,13 +38,14 @@ bool MainScene::init()
     {
         return false;
     }
+    this->gameState = GameState::Playing;
     CSLoader* instance = CSLoader::getInstance();
     instance->registReaderObject("AgentReader", (ObjectFactory::Instance) AgentReader::getInstance);
 
     
     auto rootNode = CSLoader::createNode("MainScene.csb");
     addChild(rootNode);
-    
+    this->rootNode = rootNode;
 
     
     // setup event dispatcher
@@ -79,14 +83,26 @@ void MainScene::setupKeyboardHandling()
             case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
             {
                 this->pressedKeys.left = true;
-                this->agentBody->SetLinearVelocity(b2Vec2(-1.5, 0));
+                this->agentBody->SetLinearVelocity(b2Vec2(-VELOCTY, 0));
             }
                 break;
                 
             case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
             {
                 this->pressedKeys.right = true;
-                this->agentBody->SetLinearVelocity(b2Vec2(1.5, 0));
+                this->agentBody->SetLinearVelocity(b2Vec2(VELOCTY, 0));
+            }
+                break;
+            case EventKeyboard::KeyCode::KEY_SPACE:
+            {
+                if (this->gameState == GameState::Playing)
+                {
+                    this->gameState = GameState::Paused;
+                }
+                else
+                {
+                    this->gameState = GameState::Playing;
+                }
             }
                 break;
                 
@@ -102,7 +118,7 @@ void MainScene::setupKeyboardHandling()
                 
                 if(this->pressedKeys.right)
                 {
-                    this->agentBody->SetLinearVelocity(b2Vec2(1.5, 0));
+                    this->agentBody->SetLinearVelocity(b2Vec2(VELOCTY, 0));
                 }
                 else {
                     this->agentBody->SetLinearVelocity(b2Vec2(0, 0));
@@ -115,7 +131,7 @@ void MainScene::setupKeyboardHandling()
                 
                 if(this->pressedKeys.left)
                 {
-                    this->agentBody->SetLinearVelocity(b2Vec2(-1.5, 0));
+                    this->agentBody->SetLinearVelocity(b2Vec2(-VELOCTY, 0));
                 }
                 else {
                     this->agentBody->SetLinearVelocity(b2Vec2(0, 0));
@@ -126,13 +142,30 @@ void MainScene::setupKeyboardHandling()
     this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(keyboardListener, this);
 }
 
-b2World* MainScene::getSceneWorld()
+/*
+* @desc A simple getter class to gain access to the simulation's physics world
+* @return type b2World*
+* @params: None
+*/
+b2World* MainScene::getPhysicsWorld()
 {
     b2World* world = this->sceneWorld;
     return world;
 }
 
 
+/*
+ * @desc
+ * This is where the magic happens: First, we set up our physics world.
+ * After, we initialize all bodies which are needed for simulation:
+ * + The edge boxes preventing the agent from falling off the rail
+ * + The rail on which the agent rides along
+ * + The agent itself
+ * + The pole
+ * + The revolute joint between the agent and the pole.
+ * @return type b2World*
+ * @params: None
+ */
 void MainScene::setupWorld()
 {
     Size visibleSize = Director::getInstance()->getVisibleSize();
@@ -144,33 +177,28 @@ void MainScene::setupWorld()
     // the world to the MainScene.
     b2Vec2 gravity = b2Vec2(0.0f, -10.0f);
     b2World* world = new b2World(gravity);
+
     this->setPhysicsWorld(world);
+    this->_debugDraw = new GLESDebugDraw(PTM_RATIO);
+    this->_debugDraw->SetFlags(b2Draw::e_shapeBit|b2Draw::e_jointBit|b2Draw::e_pairBit);
+    this->getPhysicsWorld()->SetDebugDraw(this->_debugDraw);
     
     
     // let's add a physically active outline to our window so that the agent cannot pass the edge of the screen
     // to do so, we first create a variable called edgeBoxDefinition of type b2BodyDef and create an instance of
     // type b2Body*, an b2EdgeShape and a b2FixtureDef.
-    
+    b2Vec2 center = b2Vec2((origin.x + visibleSize.width * 0.5) / PTM_RATIO, (origin.y + visibleSize.height * 0.5)/ PTM_RATIO);
     b2BodyDef edgeBoxBodyDefinition;
     b2Body* edgeBoxBody = world->CreateBody(&edgeBoxBodyDefinition);
     
     b2EdgeShape edgeBox;
     b2FixtureDef edgeBoxDef;
     edgeBoxDef.shape = &edgeBox;
-    
-    //top
-    edgeBox.Set(b2Vec2(origin.x  / PTM_RATIO, (origin.y + visibleSize.height) / PTM_RATIO),
-                  b2Vec2((origin.x + visibleSize.width) / PTM_RATIO, (origin.y + visibleSize.height)/ PTM_RATIO));
-    edgeBoxBody->CreateFixture(&edgeBoxDef);
-    
+    edgeBoxDef.filter.categoryBits = Categories::BOUNDARY;
+    edgeBoxDef.filter.maskBits = Categories::CART | Categories::POLE;
     
     //right
-    edgeBox.Set(b2Vec2((origin.x + visibleSize.width) / PTM_RATIO, (origin.y + visibleSize.height) / PTM_RATIO),
-                  b2Vec2((origin.x + visibleSize.width) / PTM_RATIO, origin.y / PTM_RATIO));
-    edgeBoxBody->CreateFixture(&edgeBoxDef);
-    
-    //bottom
-    edgeBox.Set(b2Vec2(origin.x / PTM_RATIO, -17.0f/PTM_RATIO), b2Vec2((origin.x + visibleSize.width)/PTM_RATIO, -17.0f/PTM_RATIO));
+    edgeBox.Set(b2Vec2((origin.x + visibleSize.width) / PTM_RATIO, (origin.y + visibleSize.height) / PTM_RATIO), b2Vec2((origin.x + visibleSize.width) / PTM_RATIO, origin.y / PTM_RATIO));
     edgeBoxBody->CreateFixture(&edgeBoxDef);
     
     //left
@@ -178,7 +206,29 @@ void MainScene::setupWorld()
     edgeBoxBody->CreateFixture(&edgeBoxDef);
     
     
+    // let's implement the rail on which our agent will move along
+    // to do so, we first create a b2BodyDef and a b2Body* and assign
+    // the rail sprite to it:
     
+    
+    // Sprite* railSprite = this->rootNode->getChildByName<Sprite*>("rail");
+    
+    b2BodyDef railBodyDef;
+    railBodyDef.type = b2_staticBody;
+    //railBodyDef.userData = railSprite;
+    railBodyDef.position.Set(center.x, (center.y - 0.08f));
+    b2Body* railBody = world->CreateBody(&railBodyDef);
+    b2PolygonShape railShape;
+    railShape.SetAsBox(2.0f, 0.02f);
+    
+    b2FixtureDef railFixtureDef;
+    railFixtureDef.shape = &railShape;
+    railFixtureDef.density = 0.0f;
+    railFixtureDef.filter.categoryBits = Categories::RAIL;
+    railFixtureDef.filter.maskBits = Categories::CART;
+    railFixtureDef.friction = 0.001f;
+    railFixtureDef.restitution = 0.0f;
+    railBody->CreateFixture(&railFixtureDef);
     
     // Now lets create our agent, define it's physical body and shape
     // To do so, we first intiantiate an agent and assign it to the scene
@@ -191,22 +241,21 @@ void MainScene::setupWorld()
     // between the agent body and sprite.
     // Finally, we create the actual pyhsical body by calling `CreateBody` on world.
     
-    b2Vec2 center = b2Vec2((origin.x + visibleSize.width * 0.5) / PTM_RATIO, (origin.y + visibleSize.height * 0.5)/ PTM_RATIO);
     this->agent = static_cast<Agent*>(CSLoader::createNode("Agent.csb"));
     Sprite* agentSprite = this->agent->getChildByName<Sprite*>("agent");
     agentSprite->setTag(1);
     agentSprite->setPosition(Vec2(center.x, center.y));
     
-    this->addChild(agentSprite);
+    //this->addChild(agentSprite);
         
     b2BodyDef agentBodyDef;
     agentBodyDef.type = b2_dynamicBody;
     agentBodyDef.fixedRotation = true;
     agentBodyDef.allowSleep = true;
-    agentBodyDef.userData = agentSprite;
+    
+    //agentBodyDef.userData = agentSprite;
     agentBodyDef.position.Set(center.x, center.y);
     b2Body* agentBody = world->CreateBody(&agentBodyDef);
-    
     
     
     // Now that we have created a physical body, we need to attach
@@ -214,45 +263,120 @@ void MainScene::setupWorld()
     // geometry. We achieve this by creating a b2PolygonShape called agentShape
     // and call the SetAsBox method. Note that SetAsBox takes the HALF-width
     // and HALF-height as its arguments.
-    // Hence, our agent will be 200px wide and 100px tall, which approximately
-    // equals a width of 200px/ 1134 (px/m) = 17.6cm and a height of
-    // 100px/ 1134 (px/m) = 8.81cm. These values are currently arbitrarily set.
+    
     
     b2PolygonShape agentShape;
-    agentShape.SetAsBox(100.0f/ PTM_RATIO, 50.0f/ PTM_RATIO);
+    agentShape.SetAsBox(AGENT_WIDTH/2, AGENT_HEIGHT/2);
     
     b2FixtureDef agentFixtureDef;
     agentFixtureDef.shape = &agentShape;
-    agentFixtureDef.density = 10.0f;
-    agentFixtureDef.friction = 0.0f;
-    agentFixtureDef.restitution = 0.1f;
+    agentFixtureDef.density = AGENT_MASS;
+    agentFixtureDef.friction = AGENT_FRICTION;
+    agentFixtureDef.restitution = AGENT_RESTITUTION;
+    agentFixtureDef.filter.categoryBits = Categories::CART;
+    agentFixtureDef.filter.maskBits = Categories::RAIL | Categories::BOUNDARY;
     agentBody->CreateFixture(&agentFixtureDef);
     
     // Call setBody on this->agent to have later access to the body
     // in case we need to modify it. Finally, schedule an update
     this->agentBody = agentBody;
+    
+    
+    // Now we have created our rail and agent, it's time to
+    // initialize our pole, which we'll later constrain to
+    // our agent. There's nothing new here to point out.
+    
+    // Pole Dimensions: 2cm in width, 30cm in length.
+    // Pole Physics:
+    // + Restitution: 0.1,
+    // + Mass: 1kg
+    
+    b2BodyDef poleBodyDef;
+    poleBodyDef.type = b2_dynamicBody;
+    poleBodyDef.allowSleep = true;
+    poleBodyDef.position.Set(center.x, (center.y - POLE_HEIGHT/2));
+    b2Body* poleBody = world->CreateBody(&poleBodyDef);
+    
+    b2PolygonShape poleShape;
+    poleShape.SetAsBox(POLE_WIDTH/2, POLE_HEIGHT/2);
+    
+    b2FixtureDef poleFixtureDef;
+    poleFixtureDef.shape = &poleShape;
+    poleFixtureDef.filter.categoryBits = Categories::POLE;
+    poleFixtureDef.filter.maskBits = Categories::BOUNDARY;
+    poleFixtureDef.density = POLE_MASS;
+    poleFixtureDef.friction = POLE_FRICTION;
+    poleFixtureDef.restitution = POLE_RESTITUTION;
+    poleBody->CreateFixture(&poleFixtureDef);
+    this->poleBody = poleBody;
+    
+    
+    // joint definition
+    b2RevoluteJointDef jointDef;
+    jointDef.bodyA = agentBody;
+    jointDef.bodyB = poleBody;
+    jointDef.localAnchorA = agentBody->GetLocalCenter();
+    
+    b2Vec2 polePosition = b2Vec2(poleBody->GetLocalCenter().x, poleBody->GetLocalCenter().y + POLE_HEIGHT/2);
+    jointDef.localAnchorB = polePosition;
+    b2RevoluteJoint* joint = static_cast<b2RevoluteJoint*>(world->CreateJoint(&jointDef));
+    
+    
     this->schedule(CC_SCHEDULE_SELECTOR(MainScene::update));
 }
 
 void MainScene::update(float dt)
 {
-    Layer::update(dt);
-    int velocityIterations =10;
-    int positionIterations = 10;
-    
-    this->getSceneWorld()->Step(dt, velocityIterations, positionIterations);
-    
-    for (b2Body* body = this->getSceneWorld()->GetBodyList(); body; body = body->GetNext())
+    if (this->gameState == GameState::Playing)
     {
-        if (body->GetUserData() != NULL) {
+        Layer::update(dt);
+        int velocityIterations =10;
+        int positionIterations = 10;
+        
+        this->getPhysicsWorld()->Step(dt, velocityIterations, positionIterations);
+        
+        CCLOG("Angle: %.2f", this->normalizeAngle(this->r2d(this->poleBody->GetAngle())));
+        for (b2Body* body = this->getPhysicsWorld()->GetBodyList(); body; body = body->GetNext())
+        {
             
-            Sprite* sprite = (Sprite*)body->GetUserData();
-
-            Vec2 position = Vec2(body->GetPosition().x * PTM_RATIO, body->GetPosition().y * PTM_RATIO);
             
-            CCLOG("Position:%.2f,%.2f", position.x, position.y);
-            sprite->setPosition(position);
-            sprite->setRotation( -1 * CC_RADIANS_TO_DEGREES(body->GetAngle()));
+            if (body->GetUserData() != NULL) {
+                
+                Sprite* sprite = (Sprite*)body->GetUserData();
+                
+                Vec2 position = Vec2(body->GetPosition().x * PTM_RATIO, body->GetPosition().y * PTM_RATIO);
+                
+                CCLOG("Position:%.2f,%.2f", position.x, position.y);
+                sprite->setPosition(position);
+                sprite->setRotation( -1 * CC_RADIANS_TO_DEGREES(body->GetAngle()));
+            }
         }
     }
+}
+
+
+void MainScene::draw(Renderer *renderer, const Mat4 &transform, uint32_t transformUpdated)
+{
+    Layer::draw(renderer, transform, transformUpdated);
+    this->getPhysicsWorld()->DrawDebugData();
+}
+
+float MainScene::normalizeAngle(float angle)
+{
+    angle = fmod(angle + 180,360);
+    if (angle < 0)
+        angle += 360;
+    return angle - 180.0f;
+}
+
+float MainScene::r2d(float r)
+{
+    float ret = r*180.0f/M_PI;
+    return ret;
+}
+
+
+void MainScene::pausePlay()
+{
+    this->gameState = GameState::Paused;
 }
--- SwingUp/Classes/MainScene.h
+++ SwingUp/Classes/MainScene.h
@@ -3,18 +3,67 @@
 
 #include "cocos2d.h"
 #include "Agent.h"
+#include "GLES-Render.h"
 #include <Box2D/Box2D.h>
 
-// set's the Pixel to Meter Ratio
+
+// Globals definition
+// ==================
+// PTM_RATIO: Pixel to meter ratio
 // As we're initiliazing our display 1134px wide, a PTM ratio
-// of 567 sets our screen width to be 2 meters
-const static float PTM_RATIO = 1134.0f;
+// of 567 sets our screen width to equal 2 meters
+
+// VELOCITY
+// Horizontal Velocity of the agent
+const static float PTM_RATIO = 1134.0f / 2.0f;
+const static float VELOCTY = 1.2f;
+
+
+// Agent attribtues
+// ==================
+// AGENT_MASS: Mass of the agent [kg]
+// AGENT_FRICTION: Friction of the agent [-]
+// AGENT_RESTITUTION: Agent restitution [-]
+// AGENT_WIDTH: Agent width [m]
+// AGENT_HEIGHT: Agent height [m]
+const static float AGENT_MASS = 1.0f;
+const static float AGENT_FRICTION = 0.001f;
+const static float AGENT_RESTITUTION = 0.0f;
+const static float AGENT_WIDTH = 0.1f;
+const static float AGENT_HEIGHT = 0.05f;
+
+// Pole attribtues
+// ==================
+// POLE_MASS: Mass of the agent [kg]
+// POLE_FRICTION: Friction of the agent [-]
+// POLE_RESTITUTION: Agent restitution [-]
+// POLE_WIDTH: Agent width [m]
+// POLE_HEIGHT: Agent height [m]
+const static float POLE_MASS = 0.1f;
+const static float POLE_FRICTION = 0.0f;
+const static float POLE_RESTITUTION = 0.0f;
+const static float POLE_WIDTH = 0.01f;
+const static float POLE_HEIGHT = 0.18;
+
+
 
 class KeyList
 {
 public:
     bool left = false;
     bool right = false;
+};
+
+enum Categories {
+    BOUNDARY = 0x0001,
+    RAIL =     0x0002,
+    CART =     0x0004,
+    POLE =     0x0008,
+};
+
+enum class GameState {
+    Playing,
+    Paused
 };
 
 class MainScene : public cocos2d::Layer
@@ -32,16 +81,24 @@ public:
     // setup keyboard handling in case we're on a pc
     void setupKeyboardHandling();
     void setupWorld();
-    
+    void pausePlay();
+    float r2d(float radiant);
+    virtual void draw(cocos2d::Renderer *renderer, const cocos2d::Mat4 &transform, uint32_t flags) override;
     
 private:
+    GLESDebugDraw* _debugDraw;
     void setPhysicsWorld(b2World* world);
-    b2World* getSceneWorld();
+    b2World* getPhysicsWorld();
     b2World* sceneWorld;
     void update(float dt) override;
     Agent* agent;
     b2Body* agentBody;
     KeyList pressedKeys;
+    float normalizeAngle(float angle);
+    cocos2d::Node* rootNode;
+    GameState gameState;
+    b2Body* poleBody;
 };
+
 
 #endif // __MainScene_SCENE_H__
